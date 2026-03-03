8f3229ad4a824704d7089aade15e3fd99090668f
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-03-03 10:36:49 +0530

    adding ignore

--- ai.js
+++ ai.js
@@ -2,7 +2,6 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Initialize Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const getStepByStepExplanation = async (questionText) => {
